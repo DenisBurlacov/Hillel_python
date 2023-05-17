c0cb17eeb06088980532f5b2cf2bb5aa7eb1be77
denis <denis055023@gmail.com>
2023-05-17 21:19:35 +0200

    finished string task

---
+++ home_tasks/home_task4/strings.py
@@ -0,0 +1,24 @@
+string_single_quotes = 'Some string wit single quotes'
+string_twice_quotes = "Some string with twice and one's quotes"
+haiky = """ 
+    Luna de agosto.
+En el portón irrumpe
+    la marejada.
+"""
+haiky_editing = "\tLuna de agosto. \nEn el portón irrumpe\n\tla marejada."
+raw_string = r'https://some/fake/url'
+long_sring = '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
+Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem 
+Ipsum.'''
+
+print(string_single_quotes)
+print(string_twice_quotes)
+print(haiky)
+print(haiky_editing)
+print()
+print(raw_string)
+print()
+print(long_sring)
